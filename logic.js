dd6af1d0be20c1e99cda1f103a4744919689b30a
✧Nachi✧ <michuucrac@gmail.com>
2026-09-14 21:06:20 -0500

    Update GIF search term for 'Hug'

--- logic.js
+++ logic.js
@@ -12,7 +12,7 @@ import {
 const GIF_TOKEN = process.env.GIF_TOKEN;
 
 const GIF_SEARCHES = {
-    Hug: "cute furry hug",
+    Hug: "babyfur hug",
     Pat: "cute furry headpat",
     Boop: "cute furry boop",
     Cuddle: "cute furry cuddle",
